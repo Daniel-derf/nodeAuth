565be047c31424e9dc1e7cadf90635344044e886
Daniel-derf <danielpardinho321@gmail.com>
2024-10-22 10:18:09 -0300

    restricted route

--- app.js
+++ app.js
@@ -107,7 +107,7 @@ app.post('/auth/login', async (req, res) => {
 })
 
 // private route
-app.get('/user/:id', async (req,res) => {
+app.get('/user/:id', checkToken, async (req,res) => {
     const id = req.params.id
 
     // excluindo a senha do usuario do retorno na busca (-password)
@@ -116,7 +116,30 @@ app.get('/user/:id', async (req,res) => {
     if (!user){
         return res.status(404).json({msg: 'usuario não encontrado'})
     }
+
+    return res.status(200).json({ user })
 })
+
+function checkToken(req, res, next){
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+
+    if(!token){
+        return res.status(401).json({msg: 'Acesso negado!'})
+    }
+
+    try {
+        const secret = process.env.SECRET
+
+        jwt.verify(token, secret)
+
+        next()
+        
+    } catch(error){
+        res.status(400).json({msg: 'token inválido'})
+    }
+    
+}
 
 const dbUser = process.env.DB_USER
 const dbPassword = process.env.DB_PASS
